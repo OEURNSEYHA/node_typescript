a7e641c00abf050a3b2af6074303b1b4f989fed7
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-03-26 23:59:07 +0700

    update

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authController from "../controllers/authController";
+import multer from "multer";
+const upload = multer();
 const router = express.Router();
 
 
 router.get("/getall/users", authController.getAll);
-router.post("/add/user", authController.addNew);
+router.post("/register", upload.none(), authController.register);
 
 
 
