5051ccd878ae0e5212f2d818aec30437e71886e6
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-03-26 23:58:37 +0700

    update

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt"
 import AuthSchema from "../models/auth";
+import { isStrongPassword } from "validator";
 
 
 const authController = {
+
+
 
   getAll: async (req: Request, res: Response) => {
     try {
@@ -19,24 +22,25 @@ const authController = {
     }
   },
 
-  addNew: async (req: Request, res: Response) => {
+  register : async (req: Request, res: Response) => {
     try {
       // Extract email from the request body
       const { username, fullname, email, password } = req.body;
 
+      if(!isStrongPassword(password)){
+        throw new Error("Please enter strong password");
+      }
       const salt = await bcrypt.genSaltSync();
       const hasPassword = await bcrypt.hashSync(password, salt);
-
-      console.log(hasPassword);
-      return;
-
+      const maxOrder = await AuthSchema.findOne({}).sort({order:-1});
+      const order = maxOrder ? maxOrder.order + 1: 1;
       // Create a new authentication record in the database
-      const auth = await AuthSchema.create({ email });
+      const auth = await AuthSchema.create({ username, fullname, email, password: hasPassword, order });
 
       // Send a JSON response with the success message and the newly created authentication record
       res.json({
         success: true,
-        message: "Authentication record created successfully",
+       
         auth,
       });
     } catch (error : any) { // Specify 'any' type for 'error'
